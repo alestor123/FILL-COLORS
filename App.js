6606059b0451446c566601bd5a1942feac9a35f5
alestor123 <alestoraldous@gmail.com>
2021-01-08 15:03:39 +0530

    added code

--- App.js
+++ App.js
@@ -1,2 +1,4 @@
 var rainbow = require('ansi-rainbow'),
+sp = '      ',
 chalk = require('chalk');
+while(true) console.log(chalk.bgBlue(sp)+chalk.bgYellowBright(sp)+chalk.bgBlueBright(sp)+chalk.bgGreen(sp)+chalk.bgRed(sp)+chalk.bgCyan(sp))
