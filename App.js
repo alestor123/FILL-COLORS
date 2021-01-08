50cee433e804271f2d5c72b2605456318a4e5909
alestor123 <alestoraldous@gmail.com>
2021-01-08 15:05:15 +0530

    added bash

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 var sp = '      ',
 chalk = require('chalk');
 while(true) console.log(chalk.bgBlue(sp)+chalk.bgYellowBright(sp)+chalk.bgBlueBright(sp)+chalk.bgGreen(sp)+chalk.bgRed(sp)+chalk.bgCyan(sp))
